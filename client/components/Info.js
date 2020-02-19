55f71c7eda3f46532156f8a207c30e5d1cfdcbaf
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-19 17:26:47 -0500

    feat: created info component

---
+++ client/components/Info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Info = () => {
+  return ( 
+    <tr>
+      <th>AAPL - </th>
+      <td>6 shares</td>
+      <td>$2043.09</td>
+    </tr>
+  );
+}
+
+export default Info;
--- client/components/Portfolio.js
+++ client/components/Portfolio.js
@@ -1,37 +1,5 @@
-import React from 'react'
-
-// const Portfolio = () => {
-//   return ( 
-//     <div className="tile is-vertical is-8">
-//       <div className="tile">
-//         <div className="tile is-parent">
-//           <article className="tile is-child notification ">
-//             <h1 className="title">Portfolio - $5943.34</h1>
-  //                     <div class="table-container">
-  //           <table class="table">
-  //           <thead>
-  //   <tr>
-  //     <th>Ticker</th>
-  //     <th>Shares</th>
-  //     <th>Price</th>
-  //   </tr>
-  // </thead>
-  // <tbody>
-  //   <tr>
-  //     <th>1</th>
-  //     <td>38</td>
-  //     <td>38</td>
-  //   </tr>
-  //   </tbody>
-  //           </table>
-  //         </div>  
-//           </article>
-
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+import React from 'react';
+import Info from './Info';
 
 const Portfolio = () => {
   return ( 
@@ -50,11 +18,7 @@ const Portfolio = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <th>AAPL - </th>
-                  <td>6 shares</td>
-                  <td>$2043.09</td>
-                </tr>
+                <Info />
               </tbody>
             </table>
           </div>  
