0fa44907ec2ed8cbd0676dca74a6e50b4791b553
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-19 22:10:03 -0500

    chore: add secure passward

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const crypto = require('crypto');
 const db =  require('../db');
 
 const User = db.define('user', { 
@@ -20,16 +21,67 @@ const User = db.define('user', {
   }, 
   password: {
     type: Sequelize.STRING,
+    // Making `.password` act like a func hides it when serializing to JSON.
+    // This is a hack to get around Sequelize's lack of a "private" option.
+    get() {
+      return () => this.getDataValue('password');
+    },
     validate: {
       notEmpty: true,
       len: [4, 10],
-      isAlphanumerica: true
+      isAlphanumeric: true
+    }
+  },
+  salt: {
+    type: Sequelize.STRING,
+    // Making `.salt` act like a function hides it when serializing to JSON.
+    // This is a hack to get around Sequelize's lack of a "private" option.
+    get() {
+      return () => this.getDataValue('salt');
     }
   },
   cash:{
     type: Sequelize.INTEGER,
     defaultValue: 500000
   }
+});
+
+/**
+ * instanceMethods
+ */
+User.prototype.correctPassword = function(candidatePwd) {
+  return User.encryptPassword(candidatePwd, this.salt()) === this.password();
+}
+
+/**
+ * classMethods
+ */
+User.generateSalt = function() {
+  return crypto.randomBytes(16).toString('base64');
+};
+
+User.encryptPassword = function(plainText, salt) {
+  return crypto
+    .createHash('RSA-SHA256')
+    .update(plainText)
+    .update(salt)
+    .digest('hex');
+};
+
+/**
+ * hooks
+ */
+const setSaltAndPassword = user => {
+  if (user.changed('password')) {
+    user.salt = User.generateSalt();
+    user.password = User.encryptPassword(user.password(), user.salt());
+  }
+}
+
+User.beforeCreate(setSaltAndPassword);
+User.beforeUpdate(setSaltAndPassword);
+User.beforeBulkCreate(users => {
+  users.forEach(setSaltAndPassword);
 });
 
 module.exports = User; 
