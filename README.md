0c32180d21f6d695e61409dde6838ae98bc81be8
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-02-23 21:15:48 -0500

    chore:write out to do list

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 - to-do:
   - Fix url parameters /transactions on logout.
+  - Display a list of common stocks with current data. 
+  - Find all the stock in signed in user and display in portfolio. 
+  - Find all the transactions for the signed in user and display in transactions. 
 
 ### SOURCES:
 https://iexcloud.io/docs/api/
--- server/api/iex.js
+++ server/api/iex.js
@@ -4,19 +4,12 @@ const axios = require('axios');
 const API_TOKEN = require('../../secret');
 
 
-
-
-
-
-
 router.get('/stock/:symbols', async (req, res, next) => { 
   try {
     const symbols = req.params.symbols;
     const queryStr = `https://sandbox.iexapis.com/stable/stock/market/batch?symbols=${symbols}&types=quote&range=1m&last=5&token=${API_TOKEN}`;
     const { data  } = await axios.get(queryStr);
-    const filteredIexData = Stock.filterIexData(data, symbols);
-  
-    
+    const filteredIexData = Stock.filterIexData(data, symbols);    
     res.json(filteredIexData);
   } catch (error) {
     next(error);
